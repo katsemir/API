04b91210dcbb66c2e00ae65cfb005d58d75fa902
katsemir <m.katdemir@istu.edu.ua>
2025-04-24 22:50:58 +0200

    SQLAlchemy

--- API.py
+++ API.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 api = Api(app)
 db = SQLAlchemy(app)
 
@@ -12,7 +13,7 @@ db = SQLAlchemy(app)
 class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    items = db.relationship("Item", backref="store", lazy=True)
+    items = db.relationship("Item", backref="store", lazy="dynamic")
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,29 +30,44 @@ class StoreResource(Resource):
         db.session.commit()
         return {"message": "Store created", "store": {"id": store.id, "name": store.name}}, 201
 
+    def get(self):
+        stores = Store.query.all()
+        return {"stores": [{"id": s.id, "name": s.name, "items": [item.id for item in s.items]} for s in stores]}
+
 class ItemResource(Resource):
     def post(self):
         data = request.get_json()
         item = Item(name=data["name"], price=data["price"], store_id=data["store_id"])
         db.session.add(item)
         db.session.commit()
-        return {"message": "Item created", "item": {"id": item.id, "name": item.name, "price": item.price}}, 201
+        return {
+            "message": "Item created",
+            "item": {
+                "id": item.id,
+                "name": item.name,
+                "price": item.price,
+                "store_id": item.store_id,
+            },
+        }, 201
 
     def get(self):
         items = Item.query.all()
-        return {"items": [{"id": i.id, "name": i.name, "price": i.price, "store_id": i.store_id} for i in items]}, 200
+        return {
+            "items": [
+                {"id": i.id, "name": i.name, "price": i.price, "store_id": i.store_id}
+                for i in items
+            ]
+        }, 200
 
 # Реєстрація ресурсів
 api.add_resource(StoreResource, "/store")
 api.add_resource(ItemResource, "/item")
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 @app.route("/")
 def home():
     return {"message": "API працює з SQLAlchemy та One-to-Many!"}
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
